64b2e5dac5c764ee7259bbb5110f8ce6e5bd4ee2
Amelia Eric-Markovic <amelia.ericmarkovic@dcmail.ca>
2023-01-20 13:46:06 -0500

    Updating Contact Us and app.js
    Updating the different fields asked in the contact form and adding the in the js change of the word product to Intrests.

--- LAB1/scripts/app.js
+++ LAB1/scripts/app.js
@@ -1,9 +1,12 @@
 console.log("JS Script wired up");
 
 function timeRedirect(e) {
-    let contactEmail = document.getElementById('validationCustom01').value;
-    let contactPassword = document.getElementById('validationCustom02').value;
-    console.log("Email: " + contactEmail + " Password: " +contactPassword);
+    let contactFirst = document.getElementById('FirstName01').value;
+    let contactLast = document.getElementById('LastName01').value;
+    let contactNumber = document.getElementById('Contact01').value;
+    let contactAddress = document.getElementById('Email01').value;
+    let contactMessage = document.getElementById('Message01').value;
+    console.log("Name: " + contactFirst + " " + contactLast + " \nContact Number: " + contactNumber + " \nEmail Address: " + contactAddress + " \nShort Message: " + contactMessage);
     e.preventDefault();
     setTimeout(function () { 
         window.location.href = "index.html";
@@ -13,4 +16,11 @@ function timeRedirect(e) {
 let submitButton = document.getElementById('submit-button');
 if(submitButton) {
     submitButton.addEventListener('click', timeRedirect, false);
+}
+
+var navLinks = document.getElementsByClassName("nav-link");
+for (var i = 0; i < navLinks.length; i++) {
+  if (navLinks[i].innerHTML == "Products") {
+    navLinks[i].innerHTML = "Interests";
+  }
 }
