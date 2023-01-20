026cb3e3d8fbedf4f41f443a8cd1bc75e0fbcc7b
Amelia Eric-Markovic <amelia.ericmarkovic@dcmail.ca>
2023-01-20 12:48:26 -0500

    Adding nav to all pages and fixing the console part

--- LAB1/scripts/app.js
+++ LAB1/scripts/app.js
@@ -3,7 +3,7 @@ console.log("JS Script wired up");
 function timeRedirect(e) {
     let contactEmail = document.getElementById('validationCustom01').value;
     let contactPassword = document.getElementById('validationCustom02').value;
-    console.log("Email: " + contactEmail + "Password: " +contactPassword);
+    console.log("Email: " + contactEmail + " Password: " +contactPassword);
     e.preventDefault();
     setTimeout(function () { 
         window.location.href = "index.html";
